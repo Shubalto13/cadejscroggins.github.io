ab65ab58c075d9b75c4c1750fccb8a8a34a6bd8e
System Administrator <root@paloalto.local>
2014-11-24 22:03:37 -0800

    don't open browser on grunt serve

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function (grunt) {
       },
       livereload: {
         options: {
-          open: true,
+          open: false,
           base: [
             '.tmp',
             '.jekyll',
@@ -67,7 +67,7 @@ module.exports = function (grunt) {
       },
       dist: {
         options: {
-          open: true,
+          open: false,
           base: [
             '<%= yeoman.dist %>'
           ]
